f9f349b723ca55a4c3e6f01c2ed09886f69338dd
DanPancho <daarcosa@puce.edu.ec>
2022-10-17 08:18:49 -0500

    Correcion clases

--- src/components/login/UILogin/InicioSesion.jsx
+++ src/components/login/UILogin/InicioSesion.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 const InicioSesion = () => {
   const { formik, onRegister } = IniciarSesion();
   return (
-    <section class="vh-100" style={{backgroundColor: '#c1ccd4'}}>
-      <div class="container py-5 h-100" >
-          <div class="row d-flex justify-content-center align-items-center h-100 ">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card bg-dark text-warning">
-                <div class=" mt-3 mb-3 container-border text-center">
-                  <Image class="rounded" src={"/img/PUCETINDERJPG.jpg"} width={"230%"} height={"150%"}/>         
+    <section className="vh-100" style={{backgroundColor: '#c1ccd4'}}>
+      <div className="container py-5 h-100" >
+          <div className="row d-flex justify-content-center align-items-center h-100 ">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-dark text-warning">
+                <div className=" mt-3 mb-3 container-border text-center">
+                  <Image className="rounded" src={"/img/PUCETINDERJPG.jpg"} width={"230%"} height={"150%"}/>         
                 </div>
                   <form className="px-5 form" onSubmit={formik.handleSubmit}>
                   <div className="mb-3">
